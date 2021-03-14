40a162d97987e118a4b5086814d43a5a424b82c8
quison1 <williamqlim@gmail.com>
2021-03-14 20:22:38 +1100

    test

--- commit.sh
+++ commit.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-cd c:/cloud/git-project/portfolio-monitor-2
+cd C:/Users/Owner/Desktop/portfolio-monitor-2
 git add --all
 timestamp() {
   date +"at %H:%M:%S on %d/%m/%Y"
 }
-git commit -am "Regular auto-commit $(timestamp)"
+git commit -am "commit from simon at $(timestamp)"
 git push origin gh-pages
